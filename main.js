fa1f521258dc63aaa71757f156113a21fafbab74
ukengo <office@friedman.com.ua>
2022-09-04 09:39:37 +0300

    add tableReestr.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 /* import { buttonBuhExport } from "./buhgalter";
 buttonBuhExport(); */
 import buhgalterExport from "./source/js/buhgalter.js";
+import tableReestrExport from "./source/js/tableReestr.js";
+
+
+
+
 buhgalterExport();
+tableReestrExport();
